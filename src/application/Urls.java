ba3ed7716b53e455fa53ee6d582491ba3775a536
tish0 <tish.peneff@gmail.com>
2017-02-23 09:45:28 -0500

    QuickLinks

---
+++ src/application/Urls.java
@@ -0,0 +1,33 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+
+
+public class Urls extends Application {
+	
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			
+			System.out.println(Thread.currentThread().getName());
+			Parent root = FXMLLoader.load(getClass().getResource("Urls.fxml"));
+			
+			Scene scene = new Scene(root,800,600);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.setFullScreen(true);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
